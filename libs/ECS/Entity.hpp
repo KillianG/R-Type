e63d4b64053cf7e84374e83fd84676d18093be43
Bernard erwan <erwan.bernard@epitech.net>
2018-10-31 17:27:40 +0100

    begin cmake

---
+++ libs/ECS/Entity.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2018
+** Epitech scolarship project (4 years remaining)
+** File description:
+**      Made on 2018/10 by ebernard
+*/
+
+#ifndef R_TYPE_ENTITY_HPP
+#define R_TYPE_ENTITY_HPP
+
+
+class Entity
+{
+public:
+	Entity();
+private:
+};
+
+
+#endif //R_TYPE_ENTITY_HPP
